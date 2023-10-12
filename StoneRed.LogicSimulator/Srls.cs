9ce469a403ddf3830c0c4c6a631cb9701ef99b62
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2023-10-12 17:25:31 +0200

    Fix scaling

--- StoneRed.LogicSimulator/Srls.cs
+++ StoneRed.LogicSimulator/Srls.cs
@@ -20,7 +20,6 @@ namespace StoneRed.LogicSimulator;
 
 internal class Srls : Game
 {
-    public const ushort CurrentSaveVersion = 1;
     private readonly ScreenManager screenManager;
     private SrlsWindow? currentSrlsWindow = null;
     private SrlsScreen? currentSrlsScreen = null;
@@ -37,6 +36,8 @@ internal class Srls : Game
     public LogicGatesManager LogicGatesManager { get; private set; } = null!;
 
     public Settings Settings { get; set; }
+
+    public const ushort CurrentSaveVersion = 1;
 
     public Srls()
     {
@@ -120,7 +121,7 @@ internal class Srls : Game
 
     protected override void Update(GameTime gameTime)
     {
-        Scale = 1f / 800 * Window.ClientBounds.Height * Settings.Scale;
+        Scale = 1f / 800 * Window.ClientBounds.Width * Settings.Scale;
         Desktop.Root.Scale = currentSrlsScreen?.ScalingEnabled == true ? new Vector2(Scale, Scale) : Vector2.One;
 
         if (Desktop.ContextMenu is not null)
